3090881e56cf0655a7014a222480529f92f02187
Estelle Fouqueteau <estelle.fqt29@outlook.fr>
2024-11-28 19:44:30 +0100

    ajout boucle for

--- pieces.js
+++ pieces.js
@@ -2,40 +2,43 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-const article = pieces[0];
+// Afficher toutes les fiches produit de la liste
+for (let i = 0; i < pieces.length; i++) {
+  const article = pieces[i];
 
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-//variable.propriété = valeur
+  const sectionFiches = document.querySelector(".fiches"); // récupération de l'élément du DOM qui accueillera les fiches // Ajouter les news éléments à la page web en utilisant un parent pour les accueillir
+  const pieceElement = document.createElement("article"); // création d'une balise dédiée à un pièce automobile
 
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-// innerText spécifi le texte de la balise
+  const imageElement = document.createElement("img");
+  imageElement.src = article.image;
+  //variable.propriété = valeur
+  const nomElement = document.createElement("h2");
+  nomElement.innerText = article.nom;
+  // innerText spécifi le texte de la balise
 
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${
-  article.prix < 35 ? "€" : "€€€" // opérateur ternaire
-})`;
-// Littéraux de gabarit = concaténer des chaînes de caractères et des variables : entre ` ` composé de var ou expressions Js ${monExpressionJs}
+  const prixElement = document.createElement("p");
+  prixElement.innerText = `Prix: ${article.prix} € (${
+    article.prix < 35 ? "€" : "€€€" // opérateur ternaire
+  })`;
+  // Littéraux de gabarit = concaténer des chaînes de caractères et des variables : entre ` ` composé de var ou expressions Js ${monExpressionJs}
 
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)"; // opérateur nullish
+  const categorieElement = document.createElement("p");
+  categorieElement.innerText = article.categorie ?? "(aucune catégorie)"; // opérateur nullish
 
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText =
-  article.description ?? "Pas de description pour le moment";
+  const descriptionElement = document.createElement("p");
+  descriptionElement.innerText =
+    article.description ?? "Pas de description pour le moment";
 
-const stockElement = document.createElement("p");
-stockElement.innerText = article.disponibilite
-  ? "En stock"
-  : "Rupture de stock";
+  const stockElement = document.createElement("p");
+  stockElement.innerText = article.disponibilite
+    ? "En stock"
+    : "Rupture de stock";
 
-// Ajouter les news éléments à la page web en utilisant un parent pour les accueillir
-const sectionFiches = document.querySelector(".fiches");
-
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(stockElement);
+  sectionFiches.appendChild(pieceElement); // rattache la balise article à la section Fiches
+  pieceElement.appendChild(imageElement);
+  pieceElement.appendChild(nomElement);
+  pieceElement.appendChild(prixElement);
+  pieceElement.appendChild(categorieElement);
+  pieceElement.appendChild(descriptionElement);
+  pieceElement.appendChild(stockElement);
+}
